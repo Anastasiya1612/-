c7ae75c859dee9b11820ba81e692e7374cad938e
Anastasiya Bugulova <renny42@yandex.ru>
2022-09-16 21:53:35 +0400

    Add Task 03

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -7,5 +7,17 @@
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
 
-Console.Write("Введите целое число: ")
-Console.WriteLine("Hello, World!");
+Console.Write("Введите целое число a: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите целое число b: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int square = numberB * numberB;
+
+if (square == numberA) {
+    Console.WriteLine("Да");
+}
+else {
+     Console.WriteLine("Нет");
+}

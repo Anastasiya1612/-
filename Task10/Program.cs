6160d6b1163c5938789a25c33c3215695d5874d8
Anastasiya Bugulova <renny42@yandex.ru>
2022-09-26 13:27:24 +0400

    Add method in Task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -8,11 +8,23 @@
 Console.Write("Введите трёхзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num > 99 && num <1000) 
+// if (num > 99 && num <1000) 
+// {
+// int result = (num / 10) % 10;
+// Console.WriteLine($"Вторая цифра: {result}");
+// }
+// else 
+// Console.WriteLine("Некорректный ввод");
+
+if (num > 99 && num <1000)
 {
-int first = num / 10;
-int result = first % 10;
-Console.WriteLine($"Вторая цифра: {result}");
+    int FindSecondDigit (int num)
+    {
+        int second = (num/ 10) % 10;
+        return second;
+    }
+    int result = FindSecondDigit(num);
+    Console.WriteLine($"Вторая цифра: {result}");
 }
 else 
 Console.WriteLine("Некорректный ввод");

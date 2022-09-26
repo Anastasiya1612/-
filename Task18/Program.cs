7959fe2a6b2ba9b0bdda7944f577dcbb40a9d69c
Anastasiya Bugulova <renny42@yandex.ru>
2022-09-27 02:29:11 +0400

    Add Task 18

---
+++ Task18/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти: ");
+string quarterNum = Convert.ReadLine();
+
+string FindRange(int num)
+{
+    string phrase = "Диапазон возможных координат точек находится в пределах ";
+     if(num == "1") return $"{phrase} X > 0 и Y > 0";
+     if(num == "2") return $"{phrase} X < 0 и Y > 0";
+     if(num == "3") return $"{phrase} X < 0 и Y < 0";
+     if(num == "4") return $"{phrase} X > 0 и Y < 0";
+     return "Некорректный ввод";
+}
+
+string range = FindRange(quarterNum);
+Console.WriteLine(range);

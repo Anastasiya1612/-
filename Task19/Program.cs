58cb34e78dd709ecafdc346c4b57e89eb0f7a196
Anastasiya Bugulova <renny42@yandex.ru>
2022-10-04 00:57:25 +0400

    Change in Task 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -17,24 +17,12 @@ int NumberTurn(int n)
     int third = (n / 100) % 10;
     int second = (n / 1000) % 10;
     int first = (n / 10000) % 10;
-    return fifth*10000 + fourth*1000 + third*100 + second*10 + first;
+    return fifth * 10000 + fourth * 1000 + third * 100 + second * 10 + first;
 }
 if (num > 9999 && num < 100000)
-{ 
-int numberTurn = NumberTurn(num);
-if (num == numberTurn) Console.Write("Да");
-else Console.Write("Нет");
+{
+    int numberTurn = NumberTurn(num);
+    if (num == numberTurn) Console.Write("Да");
+    else Console.Write("Нет");
 }
 else Console.Write("Некорректный ввод");
-
-
-
-// Console.Write(num % 10);
-// while ((num = num / 10)!=0)
-// {
-// int turn = num % 10;
-// Console.Write(turn);
-// }
-
-// if (num == turn) Console.Write("Да");
-// else Console.Write("Нет");

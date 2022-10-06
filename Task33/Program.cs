329dfb193352dbdc2475ea728e226585b5720a95
Anastasiya Bugulova <renny42@yandex.ru>
2022-10-06 15:33:32 +0400

    Add Task 33

---
+++ Task33/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int [] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void FindElement (int [] array, int findInt)
+{
+    int result = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == findInt) result = 1;
+    }
+    if (result == 1) Console.Write("В массиве есть данный элемент");
+    else Console.Write("В массиве нет данного элемента");
+}
+
+int[] arr = CreateArrayRndInt(5, -9, 9);
+PrintArray(arr);
+
+Console.WriteLine("Введите элемент для поиска: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+FindElement(arr, number);
